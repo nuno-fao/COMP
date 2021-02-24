96a9ff00e3767b096144fba6a5a0ba13681610e2
Nunation <oliveiranunopt@gmail.com>
2021-02-24 20:56:52 +0000

    yo

---
+++ exercises/folha1/src/calculator/MyConstants.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class MyConstants {
+	public static final int ADD =1;
+	public static final int SUB =2;
+	public static final int MUL =3;
+	public static final int DIV =4;
+	public static final char[] ops = {'+','-','*','/'};
+}
