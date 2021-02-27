b78a6d98af09a93eb189822eb23cc5108f5f830d
Nunation <oliveiranunopt@gmail.com>
2021-02-27 18:06:44 +0000

    added ex2

--- TP/IntroToJavaCC/src/ex1/MyConstants.java
+++
@@ -1,9 +0,0 @@
-package ex1;
-
-public class MyConstants {
-	public static final int ADD =1;
-	public static final int SUB =2;
-	public static final int MUL =3;
-	public static final int DIV =4;
-	public static final char[] ops = {'+','-','*','/'};
-}
