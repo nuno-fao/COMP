0f8c715728b3b548169585b4555215598253dcd4
Nunation <oliveiranunopt@gmail.com>
2021-02-27 15:53:21 +0000

    renamed folder, finished example exercise

---
+++ TP/IntroToJavaCC/src/calculator/CalculatorTreeConstants.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public interface CalculatorTreeConstants {
+	public int JJTEXPRESSION = 0;
+	public int JJTVOID = 1;
+	public int JJTADD = 2;
+	public int JJTSUB = 3;
+	public int JJTMUL = 4;
+	public int JJTDIV = 5;
+	public int JJTTERM = 6;
+	public String[] jjtNodeName = { "Expression",
+	"void", "Add", "Sub", "Mul", "Div", "Term" };
+}
